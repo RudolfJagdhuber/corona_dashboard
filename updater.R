7fec65c80a7c3e4886870ff99efcfe01867d9714
RudolfJagdhuber <r.jagdhuber@gmail.com>
2022-03-27 09:52:57 +0200

    minor typo

--- updater.R
+++ updater.R
@@ -1,7 +1,8 @@
 # Helper to fetch the most recent RKI and WHO data sets, format and store it
 
+# Note: I create a custom lib.loc on github to have writing access
 library(data.table, lib.loc = "./tmpLib")
-library(sparkline, lib.loc = "./tmpLib"))
+library(sparkline, lib.loc = "./tmpLib")
 
 # function to compute vector of summed infections for last n days
 # ni = New infections vector sorted by date (newest first!)
